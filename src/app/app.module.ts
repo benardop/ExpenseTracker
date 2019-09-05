7ae6181b4fce75b0e152f3dd2928b70108b3bf29
Benard Pacho <benard.oc.p@gmail.com>
2019-09-06 01:32:11 +0300

    add top bar module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from '@src/app/app-routing.module';
+// import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
-import { HomeComponent } from '@src/app/home/home.component';
+// import { HomeComponent } from '@src/app/home/home.component';
+import { TopBarComponent } from './home/top-bar/top-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    BrowserModule
+    // AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+     NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from '@src/app/home/home.component';
+// import { HomeComponent } from '@src/app/home/home.component';
 
 export const routes: Routes = [
   {
@@ -9,7 +9,6 @@ export const routes: Routes = [
       pathMatch: 'full',
   },
   {
-      path: 'home',
-      component: HomeComponent,
+      path: 'home'
   },
 ];
