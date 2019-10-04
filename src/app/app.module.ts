03a31cef8b18a52d542bef0a0c04ad85066995a9
Benard Pacho <benard.oc.p@gmail.com>
2019-10-04 15:33:10 +0300

    add data entry form and table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,33 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import bootstrap from 'bootstrap';
 
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
-import { ExpensesListComponent } from './expenses-list/expenses-list.component';
+import { ExpensesListComponent } from '@src/app/expenses-list/expenses-list.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { ExpenseFormComponent } from '@src/app/expense-form/expense-form.component';
+import { from } from 'rxjs';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ExpensesListComponent
+    ExpensesListComponent,
+    ExpenseFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
